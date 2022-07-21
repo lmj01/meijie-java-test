2cce69527be4798dfbc501829986dc245dbc8373
lmj01 <lmj01@users.noreply.github.com>
2022-07-21 12:11:44 +0800

    update

--- doc/configure.md
+++ doc/configure.md
@@ -4,6 +4,18 @@
 https://spring.io/quickstart
 参考上面执行
 
+重新在线配置环境，
+com.meijie
+mjapp
+增加依赖
+spring session
+spring web
+spring data jpa
+spring boot dev tools
+spring security
+
+增加的依赖暂时没有逻辑，需要注释掉，否则会启动登录验证
+
 2022-07-20
 加入依赖com.itext
 创建两个基本的类
@@ -14,3 +26,4 @@ https://spring.io/quickstart
 packaging is war
 Java is 8
 spring boot is 2.7.1
+
--- src/main/java/com/meijie/meijiejavatest/MeijieJavaTestApplication.java
+++ src/main/java/com/meijie/meijiejavatest/MeijieJavaTestApplication.java
@@ -2,11 +2,15 @@ package com.meijie.meijiejavatest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+/**
+ * 配置数据库了，但是没有使用，启动时先屏蔽掉数据库的配置
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @RestController
 public class MeijieJavaTestApplication {
 	
