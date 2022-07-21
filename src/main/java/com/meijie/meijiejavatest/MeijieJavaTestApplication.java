f515e0eec77dc438bcdd5f1ce9e6b175b0b58017
lmj01 <lmj01@users.noreply.github.com>
2022-07-21 10:07:14 +0800

    update the git init and project demo

--- src/main/java/com/meijie/meijiejavatest/MeijieJavaTestApplication.java
+++ src/main/java/com/meijie/meijiejavatest/MeijieJavaTestApplication.java
@@ -2,13 +2,21 @@ package com.meijie.meijiejavatest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MeijieJavaTestApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeijieJavaTestApplication.class, args);
 		System.out.println("meijie java test");
 	}
-
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
